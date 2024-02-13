76210a6dc29aabc75ea3abe2249424e1cb7d895d
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-02-14 00:01:30 +0100

    chore(compose): add the LazyVerticalStaggeredGrid in order to display notes

--- app/src/main/java/com/openclassrooms/notes/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/notes/MainActivity.kt
@@ -1,24 +1,26 @@
 package com.openclassrooms.notes
 
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Note
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FabPosition
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.openclassrooms.notes.repository.NotesRepository
 import com.openclassrooms.notes.ui.theme.NotesTheme
@@ -110,6 +112,33 @@ private fun Notes(
     modifier: Modifier = Modifier,
     notesRepository: NotesRepository
 ) {
-    val notes by notesRepository.notes.collectAsStateWithLifecycle(emptyList<Note>())
+    val notes by notesRepository.notes.collectAsStateWithLifecycle(emptyList())
+
+    LazyVerticalStaggeredGrid(
+        modifier = Modifier.fillMaxSize(),
+        columns = StaggeredGridCells.Fixed(LocalContext.current.resources.getInteger(R.integer.span_count)),
+        contentPadding = PaddingValues(16.dp),
+        verticalItemSpacing = 16.dp,
+        horizontalArrangement = Arrangement.spacedBy(16.dp),
+        content = {
+            items(
+                items = notes,
+            ) {
+                NoteItem(it)
+            }
+        }
+    )
+}
+
+@Composable
+private fun NoteItem(note: Pair<String, String>) {
 
 }
+@Preview(showBackground = true)
+@Composable
+private fun NoteItemPreview() {
+    NotesTheme(dynamicColor = false) {
+        NoteItem(note = Pair("Title", "Super loooooong description with a lot of words!"))
+    }
+}
+
