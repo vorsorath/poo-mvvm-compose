bc4486a91f3937f36afb9f21db4c6dbe20884d8f
Ludovic Roland <ludovic.c.roland@gmail.com>
2024-02-13 23:45:22 +0100

    chore(compose): Start the migration of the MainActivity on compose + clean-up the code

--- app/src/main/java/com/openclassrooms/notes/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/notes/MainActivity.kt
@@ -1,79 +1,124 @@
 package com.openclassrooms.notes
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.lifecycleScope
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
-import com.openclassrooms.notes.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.openclassrooms.notes.repository.NotesRepository
-import com.openclassrooms.notes.widget.NoteItemDecoration
-import com.openclassrooms.notes.widget.NotesAdapter
-import kotlinx.coroutines.launch
+import com.openclassrooms.notes.ui.theme.NotesTheme
 
 /**
  * The main activity for the app.
  */
-class MainActivity : AppCompatActivity() {
-
-    /**
-     * The binding for the main layout.
-     */
-    private lateinit var binding: ActivityMainBinding
-
-    private val notesAdapter = NotesAdapter(emptyList())
+class MainActivity : ComponentActivity() {
 
     private val notesRepository = NotesRepository()
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+//
+//        initRecyclerView()
+//        initFABButton()
+//        collectNotes()
 
-        initRecyclerView()
-        initFABButton()
-        collectNotes()
-    }
-
-    /**
-     * Collects notes from the repository and updates the adapter.
-     */
-    private fun collectNotes() {
-        lifecycleScope.launch {
-            notesRepository.notes.collect {
-                notesAdapter.updateNotes(it)
+        setContent {
+            NotesTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(stringResource(id = R.string.app_name))
+                            }
+                        )
+                    },
+                    floatingActionButtonPosition = FabPosition.End,
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            onClick = {
+                                //TODO
+                            }
+                        ) {
+                            Icon(
+                                imageVector = Icons.Filled.Add,
+                                contentDescription = "Add a note"
+                            )
+                        }
+                    }
+                ) {
+                    Notes(
+                        modifier = Modifier.padding(it),
+                        notesRepository = notesRepository
+                    )
+                }
             }
         }
     }
 
-    /**
-     * Initializes the FAB button.
-     */
-    private fun initFABButton() {
-        binding.btnAdd.setOnClickListener {
-            MaterialAlertDialogBuilder(this).apply {
-                setTitle(R.string.coming_soon)
-                setMessage(R.string.not_available_yet)
-                setPositiveButton(android.R.string.ok, null)
-            }.show()
-        }
-    }
+//    /**
+//     * Collects notes from the repository and updates the adapter.
+//     */
+//    private fun collectNotes() {
+//        lifecycleScope.launch {
+//            notesRepository.notes.collect {
+//                notesAdapter.updateNotes(it)
+//            }
+//        }
+//    }
+//
+//    /**
+//     * Initializes the FAB button.
+//     */
+//    private fun initFABButton() {
+//        binding.btnAdd.setOnClickListener {
+//            MaterialAlertDialogBuilder(this).apply {
+//                setTitle(R.string.coming_soon)
+//                setMessage(R.string.not_available_yet)
+//                setPositiveButton(android.R.string.ok, null)
+//            }.show()
+//        }
+//    }
+//
+//    /**
+//     * Initializes the RecyclerView.
+//     */
+//    private fun initRecyclerView() {
+//        with(binding.recycler) {
+//            addItemDecoration(
+//                NoteItemDecoration(
+//                    resources.getDimensionPixelSize(R.dimen.default_margin),
+//                    resources.getInteger(R.integer.span_count)
+//                )
+//            )
+//
+//            adapter = notesAdapter
+//        }
+//
+//    }
 
-    /**
-     * Initializes the RecyclerView.
-     */
-    private fun initRecyclerView() {
-        with(binding.recycler) {
-            addItemDecoration(
-                NoteItemDecoration(
-                    resources.getDimensionPixelSize(R.dimen.default_margin),
-                    resources.getInteger(R.integer.span_count)
-                )
-            )
+}
 
-            adapter = notesAdapter
-        }
-
-    }
+@Composable
+private fun Notes(
+    modifier: Modifier = Modifier,
+    notesRepository: NotesRepository
+) {
 
 }
